366c8e9394990d0bfbacdba2199055997f465e1c
Junhong Liu (Worldwide Field Ops) <junliu@nvidia.com>
2023-03-28 19:38:27 +0800

    Revert "add append unique op"
    This reverts commit eb8024dfe950c5fbbb13576fe6e7303554d2a3c2.

--- cpp/include/wholememory/wholegraph_op.h
+++ cpp/include/wholememory/wholegraph_op.h
@@ -16,7 +16,7 @@ extern "C" {
  * @param wm_csr_col_ptr_tensor : Wholememory Tensor of graph csr_col_ptr
  * @param center_nodes_tensor : None Wholememory Tensor of center node to sample
  * @param max_sample_count : maximum sample count
- * @param output_sample_offset_tensor : Wholememory Tensor of output sample offset
+ * @param output_sample_offset_tensor : pointer to output sample offset
  * @param output_dest_memory_context : memory context to output dest nodes
  * @param output_center_localid_memory_context : memory context to output center local id
  * @param output_edge_gid_memory_context : memory context to output edge global id
@@ -46,7 +46,7 @@ wholememory_error_code_t wholegraph_csr_unweighted_sample_without_replacement(
  * @param wm_csr_weight_ptr_tensor : Wholememory Tensor of graph edge weight
  * @param center_nodes_tensor : None Wholememory Tensor of center node to sample
  * @param max_sample_count : maximum sample count
- * @param output_sample_offset_tensor : Wholememory Tensor of output sample offset
+ * @param output_sample_offset_tensor : pointer to output sample offset
  * @param output_dest_memory_context : memory context to output dest nodes
  * @param output_center_localid_memory_context : memory context to output center local id
  * @param output_edge_gid_memory_context : memory context to output edge global id
